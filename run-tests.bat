cf8e1145c28ce98200668783ab381e2def5277a0
Erin O'Connell <erinocon5@gmail.com>
2018-06-10 16:34:29 -0600

    squash

--- run-tests.bat
+++ run-tests.bat
@@ -5,3 +5,4 @@ R:\.venv\Scripts\pip install -e . --upgrade --upgrade-strategy=only-if-needed
 R:\.venv\Scripts\pipenv install --dev
 
 SET RAM_DISK=R:&& SET PYPI_VENDOR_DIR=".\tests\pypi\" && R:\.venv\Scripts\pipenv run pytest -n auto -v tests --tap-stream > report.tap
+
